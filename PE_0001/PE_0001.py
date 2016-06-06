fbc363058cf04d5cb7207a730bc471638cf6c74c
mbh038 <mike038@btinternet.com>
2016-06-06 06:57:50 +0100

    p1

--- PE_0001/PE_0001.py
+++ PE_0001/PE_0001.py
@@ -16,7 +16,7 @@ def PE_0001(n):
     #allints=np.arange(n)
     #muls3and5=np.logical_or(allints/3.-allints/3==0,allints/5.-allints/5==0)
     print("--- %s seconds ---" % (time.time() - start_time))
-    return sum(np.arange(3,n,3))+sum(np.arange(5,n,5))-sum(np.arange(15,n,15))
+    return __builtins__.sum(np.arange(3,n,3))+__builtins__.sum(np.arange(5,n,5))-__builtins__.sum(np.arange(15,n,15))
     #return allints.dot(muls3and5)
     
     
