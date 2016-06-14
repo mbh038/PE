d5ffdd6aa7d36d52f5f8c3946738906b35946d34
mbh038 <mike038@btinternet.com>
2016-06-14 07:01:22 +0100

    p4,5,19

--- PE_0004/PE_0004.m
+++ PE_0004/PE_0004.m
@@ -2,38 +2,36 @@ function [ n ] = PE_0004( dig,lim )
 %PALIN_PRODUCT Summary of this function goes here
 %   Detailed explanation goes here
 
-nums=10^(dig-1):10^dig-1;
+nums=10^dig-1:-1:10^(dig-1);
 
-count=0;
-prod=zeros(length(nums));
-for i=1:length(nums)
-    for j=1:length(nums)
-        prod(i,j)=nums(i)*nums(j);
-    end
-end
-
-produ=unique(prod);
-produ=produ(:);
-
-palins=zeros(length(produ),1);
-count=0;
-tic
-
-for i=1:length(produ)
-    str=num2str(produ(i));
-    strinv=flip(str);
-    if str==strinv
-        count=count+1;
-        palins(count)=str2num(str);
-    end
-end
-toc
-
-
-if lim <min(palins)
+produ=nums'*nums;
+produ=produ(produ<lim);
+if isempty(produ)
     n=0;
-else
-    n=max(palins(palins<lim));
+    return
+end
+produ=sort(produ,'descend');
+
+for i= 1:length(produ)
+    if is_pal(produ(i))
+        n=produ(i);
+        return
+    end
+    n=0;
 end
 
+
+end
+
+function pal=is_pal(n) 
+  if (mod(n,10) == 0) 
+      pal=false;
+      return
+  end
+  r = 0;
+  while (r < n) 
+    r = 10 * r + mod(n,10);
+    n =floor(n/10);
+  end
+  pal= n == r || n == floor(r / 10);
 end
---
+++ PE_0019/PE_0019.m
@@ -0,0 +1,30 @@
+function [ output_args ] = PE_0019( dow,year_begin,year_end )
+%PE_0019 Summary of this function goes here
+%   day: day of week (1 - Sunday through to 7 - Saturday): positive integer 1-7
+%   year_begin: 01-01 of beginning year: positive integer
+%   year_end:: 31-12 ofend year: positive integer > year_begin
+
+% test for valid input:dow
+if ~isscalar(dow)  || dow <1 || dow > 7 || dow ~= floor(dow)
+    fprintf('Invalid input for year_begin. Enter a positive integer ')
+    return
+end
+
+% test for valid input:year_begin
+if ~isscalar(year_begin) || year_begin ~= floor(year_begin)
+    fprintf('Invalid input for year_begin. Enter a positive integer ')
+    return
+end
+
+% test for valid input:year_end
+if ~isscalar(year_end) || year_end ~= floor(year_end) || year_end < year_begin
+    fprintf('Invalid input for year_end. Enter a positive integer >= year_begin ')
+    return
+end
+
+foms=0
+for y=year_begin:year_end
+foms=foms+sum(weekday(datetime(y,1:12,dow))==1)
+end
+
+end
