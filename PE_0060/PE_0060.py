fdf841ef3d05c0eaf25e9b4717557bdf2068288c
mbh038 <mbh038@gmail.com>
2016-08-30 11:43:56 +0100

    p60

--- PE_0060/PE_0060.py
+++ PE_0060/PE_0060.py
@@ -25,46 +25,72 @@ import math
 import primes as p
 from timeit import default_timer as timer
 
-def goodprimes(n):
+def goodprimes(n,m):
+    start=timer()
     primes=list(primesfrom2to(n))
     [primes.remove(x) for x in [2,5]]     
-    pdic={}
+    pdic={prime:set([prime]) for prime in primes}
+    print(pdic)
+#    primes2=primes[:]
     for prime1 in primes:
-        bad=0        
-        p1set=set([prime1])
-        
-        for prime2 in primes:
-            if prime2==prime1:
-                continue 
+        print (prime1)
+#        bad=0        
+#        p1set=set([prime1])
+#        print(len(primes.difference(pdic[prime1])),len(primes))
+        for prime2 in primes:#.difference(pdic[prime1]):
+#            print(prime1,prime2,pdic[prime2],primes.difference(pdic[prime1]))
+#            print (prime2,prime1,pdic[prime1])
+            
+#            if prime2 in pdic[prime1]:
+#                continue
+#            if prime2==prime1:
+#                continue 
             try1=int((str(prime1)+str(prime2)))
             if not p.is_prime1(try1):
-                bad+=1
+#                bad+=1
                 continue
             try2=int((str(prime2)+str(prime1)))
             if not p.is_prime1(try2):
-                bad+=1
+#                bad+=1
                 continue
-            p1set.add(prime2)
-        pdic[prime1]=p1set
+#            p1set.add(prime2)
+            pdic[prime1].add(prime2)
+#            pdic[prime2].add(prime1)
+#        print (pdic[prime1])
+
+#        pdic[prime1]=p1set
+#        print(prime1,prime2,pdic)
+#        print(prime1,prime2,pdic[prime2],primes.difference(pdic[prime1]))
+#        break
+        if len (pdic[prime1])<m:
+            print([(k,v) for k,v in pdic.items() if k==prime1])
+            primes.remove(prime1)
+            del(pdic[prime1])    
+    print ('Elapsed time:',timer()-start)
+#    print(sum([len(v) for k,v in pdic.items()])/len(pdic))
     return pdic
            
 def PE_0060(n,m):
     
     start=timer()
     
-    pdic=goodprimes(n)
+    pdic=goodprimes(n,m)
     print ('Elapsed time:',timer()-start)
     start=timer()
     smin=math.inf
     for k,v in pdic.items():
-        for x in v: 
-            if len(v.intersection(pdic[x]))==m:
-                tt=v.intersection(pdic[x])
-                btest=[p.is_prime1(int(str(y1)+str(y2))) for y1 in tt for y2 in tt if y1!=y2]
-                if False not in btest:# and sum(tt)<smin:
+        for x in v:
+            if x ==k: continue
+            tt=v.intersection(pdic[x])
+            
+            if len(tt)==m:
+#                print(tt)
+                if sum([(p.is_prime1(int(str(x)+str(y)))) for x in tt for y in tt if x!=y])==len(tt)*(len(tt)-1):
                     if sum(tt)<smin:
                         smin=sum(tt)
-                        print(tt,smin)
+                        ttmin=tt
+    print(ttmin,smin)
+
        
     print ('Elapsed time:',timer()-start)
     
